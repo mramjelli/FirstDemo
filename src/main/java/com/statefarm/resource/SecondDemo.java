c297a439ad5202217007ff3687fa2f93f662ce19
mramjelli <mramjelli@gmail.com>
2018-07-10 23:25:20 -0500

    commit

---
+++ src/main/java/com/statefarm/resource/SecondDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.statefarm.resource;
+
+/**
+ * @author mramj
+ *
+ */
+public class SecondDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x=10, y=20;
+			int z;
+			z=x+y;
+			System.out.println(+z);
+		}
+
+}

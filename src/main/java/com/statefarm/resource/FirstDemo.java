dba550d5a7f43053cecf7affef13e67bbd84b5ff
mramjelli <mramjelli@gmail.com>
2018-07-10 23:11:21 -0500

    Commit

---
+++ src/main/java/com/statefarm/resource/FirstDemo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.statefarm.resource;
+
+/**
+ * @author mramj
+ *
+ */
+public class FirstDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First program");
+
+	}
+
+}
